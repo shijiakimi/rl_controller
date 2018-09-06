eca1fb9c61fc60911e760453bfd37ee3e161837e
jiashi <shijiakimi@gmail.com>
2018-09-06 13:57:18 -0400

    debug

--- env1.py
+++ env1.py
@@ -149,7 +149,7 @@ class ArmEnv(object):
     def reset(self):
         self.uav_euler = np.zeros(3)
         self.uav_pos = 1.0 * np.random.rand(3)
-        self.uav_init_pos = self.uav_pos
+        self.uav_init_pos = np.array(list(self.uav_pos))
         self.uav_v = np.zeros(3)
         self.uav_w = np.zeros(3)
         self.prop_wind_speed = np.zeros(4)
