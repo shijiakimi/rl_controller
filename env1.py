e24aab1b6d1ccb5e154a11de7d0e3752f497d40c
jiashi <shijiakimi@gmail.com>
2018-10-14 15:17:16 -0400

    try different clip

--- env1.py
+++ env1.py
@@ -38,8 +38,8 @@ class ArmEnv(object):
 
     def step(self, action):
         done = False
-        action = np.clip(action, *self.action_clip)
-        #print 'action', action
+        action = np.clip(action, self.action_clip[0], self.action_clip[1])
+        print 'action', action
         self.get_prop_wind_speed()
         thrusts = self.get_thrust(action)
         #print 'thrust', thrusts
