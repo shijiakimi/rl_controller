d497182792b813677c4637bdfcac64e72d20b009
jiashi <shijiakimi@gmail.com>
2018-10-14 17:44:17 -0400

    change mem cap to 15000, change max step to 200, change action upper bound to 5000

--- env1.py
+++ env1.py
@@ -6,7 +6,7 @@ import math
 class ArmEnv(object):
     dt = .02
     action_bound = [0, 1]
-    action_clip = [1, 3000]
+    action_clip = [1, 5000]
     goal = {'x': 0.5, 'y': 0.5, 'z': 5, 'l': 0.02}
     state_dim = 10
     action_dim = 4
