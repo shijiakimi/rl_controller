23ea47cdc89d3cec11831c9adec1c6ca239e8a8d
jiashi <shijiakimi@gmail.com>
2018-10-14 15:01:26 -0400

    remove print rl learn

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def train():
             ep_r += r
             if rl.memory_full:
                 # start to learn once has fulfilled the memory
-                print "rl.learn"
+                # print "rl.learn"
                 rl.learn()
 
             s = s_
