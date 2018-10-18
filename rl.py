0b0eedfa8ea441016299b5bf9ac2dbba25595a65
jiashi <shijiakimi@gmail.com>
2018-10-18 11:04:23 -0400

    change activation to relu

--- rl.py
+++ rl.py
@@ -88,19 +88,19 @@ class DDPG(object):
 
     def _build_a(self, s, scope, trainable):
         with tf.variable_scope(scope):
-            net = tf.layers.dense(s, 64, activation=tf.nn.tanh, name='l1', trainable=trainable)
-            net1 = tf.layers.dense(net,64, activation = tf.nn.tanh, name = 'l2', trainable = trainable)
+            net = tf.layers.dense(s, 100, activation=tf.nn.relu, name='l1', trainable=trainable)
+            net1 = tf.layers.dense(net,100, activation = tf.nn.relu, name = 'l2', trainable = trainable)
             a = tf.layers.dense(net1, self.a_dim, activation=tf.nn.relu, name='a', trainable=trainable)
             bounded_a = self.a_bound[0] + tf.nn.sigmoid(a) * (self.a_bound[1] - self.a_bound[0])
             return bounded_a
 
     def _build_c(self, s, a, scope, trainable):
         with tf.variable_scope(scope):
-            n_l1 = 64
+            n_l1 = 100
             w1_s = tf.get_variable('w1_s', [self.s_dim, n_l1], trainable=trainable)
             w1_a = tf.get_variable('w1_a', [self.a_dim, n_l1], trainable=trainable)
             b1 = tf.get_variable('b1', [1, n_l1], trainable=trainable)
-            net = tf.nn.tanh(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
+            net = tf.nn.relu(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
             net1 = tf.layers.dense(net, n_l1, activation=tf.nn.relu, name = 'net1', trainable = trainable)
             return tf.layers.dense(net1, 1, trainable=trainable)  # Q(s,a)
 
