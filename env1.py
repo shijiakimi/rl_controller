0e865a5d9f58b13f11cdb837a2e2eecbc574eddc
jiashi <shijiakimi@gmail.com>
2018-10-31 16:54:17 -0400

    change action dim to 1

--- env1.py
+++ env1.py
@@ -40,7 +40,7 @@ class ArmEnv(object):
         done = False
         #action = np.clip(action, self.action_clip[0], self.action_clip[1])
         #print 'action', action
-        action = [action_] * 4
+        action = [action_[0]] * 4
         print action
         self.get_prop_wind_speed()
         thrusts = self.get_thrust(action)
