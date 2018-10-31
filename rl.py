c0cfd0fce4b76c255d6b172c46861ed731147a9b
jiashi <shijiakimi@gmail.com>
2018-10-31 16:57:14 -0400

    change nn to 400 nodes and activation for q is tanh

--- rl.py
+++ rl.py
@@ -88,21 +88,21 @@ class DDPG(object):
 
     def _build_a(self, s, scope, trainable):
         with tf.variable_scope(scope):
-            net = tf.layers.dense(s, 200, activation=tf.nn.relu, name='l1', trainable=trainable)
-            net1 = tf.layers.dense(net,200, activation = tf.nn.relu, name = 'l2', trainable = trainable)
+            net = tf.layers.dense(s, 400, activation=tf.nn.relu, name='l1', trainable=trainable)
+            net1 = tf.layers.dense(net,400, activation = tf.nn.relu, name = 'l2', trainable = trainable)
             a = tf.layers.dense(net1, self.a_dim, activation=tf.nn.relu, name='a', trainable=trainable)
             bounded_a = self.a_bound[0] + tf.nn.sigmoid(a) * (self.a_bound[1] - self.a_bound[0])
             return bounded_a
 
     def _build_c(self, s, a, scope, trainable):
         with tf.variable_scope(scope):
-            n_l1 = 200
+            n_l1 = 400
             w1_s = tf.get_variable('w1_s', [self.s_dim, n_l1], trainable=trainable)
             w1_a = tf.get_variable('w1_a', [self.a_dim, n_l1], trainable=trainable)
             b1 = tf.get_variable('b1', [1, n_l1], trainable=trainable)
             net = tf.nn.relu(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
-            net1 = tf.layers.dense(net, n_l1, activation=tf.nn.relu, name = 'net1', trainable = trainable)
-            return tf.layers.dense(net1, 1, trainable=trainable)  # Q(s,a)
+            net1 = tf.layers.dense(net, n_l1, activation=tf.nn.tanh, name = 'net1', trainable = trainable)
+            return tf.layers.dense(net1, 1, activation = tf.nn.tanh, trainable=trainable)  # Q(s,a)
 
     def save(self):
         saver = tf.train.Saver()
