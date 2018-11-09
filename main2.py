4f0bcecd7eb7123b4cf5b2a23d299d1ef499dc77
jiashi <shijiakimi@gmail.com>
2018-11-08 19:35:33 -0500

    debug print info

--- main2.py
+++ main2.py
@@ -18,5 +18,5 @@ for i in range(max_episodes):
         agent.step(action, reward, next_state, done)
         state = next_state
         if done:
-            print('Ep: %i | %s | ep_r: %.1f | step: %i | %.2f, %.2f, %.2f | %.2f, %.2f, %.2f | %.2f, %.2f, %.2f'% (i, '---' if not done else 'done', reward, step, task.sim.init_pose[0], task.sim.init_pose[1], task.sim.init_pose[2], task.sim.pose[0], task.sim.pose[1], task.sim.pose[2], task.sim.pose[0] - task.sim.init_pos[0], task.sim.pose[1] - task.sim.init_pos[1], task.sim.pose[2] - task.sim.init_pos[2]))
+            print('Ep: %i | %s | ep_r: %.1f | step: %i | %.2f, %.2f, %.2f | %.2f, %.2f, %.2f | %.2f, %.2f, %.2f'% (i, '---' if not done else 'done', reward, step, task.sim.init_pose[0], task.sim.init_pose[1], task.sim.init_pose[2], task.sim.pose[0], task.sim.pose[1], task.sim.pose[2], task.sim.pose[0] - task.sim.init_pose[0], task.sim.pose[1] - task.sim.init_pose[1], task.sim.pose[2] - task.sim.init_pose[2]))
             break
