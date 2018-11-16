2ed0fe95a6ecd94bc990f24b548d39da2f936e5e
jiashi <shijiakimi@gmail.com>
2018-11-15 19:07:47 -0500

    debug

--- env1.py
+++ env1.py
@@ -51,6 +51,7 @@ class ArmEnv(object):
         #print 'thrust', thrusts
         linear_acc = self.get_linear_forces(thrusts) / self.mass
         #print 'linear_acc', linear_acc
+        print type(self.uav_v), type(self.dt), type(linear_acc)
         self.uav_pos += self.uav_v * self.dt + 0.5 * linear_acc * self.dt ** 2
         #print 'pos', self.uav_pos
         self.uav_v += linear_acc * self.dt
